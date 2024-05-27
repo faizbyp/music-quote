82175d0a6243f2d6111d835d0f9a99d4fa722a23
faizbyp <faizbyp@gmail.com>
2024-05-27 07:49:41 +0700

    install bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '@fontsource-variable/inter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
