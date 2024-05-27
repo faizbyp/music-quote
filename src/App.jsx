3812b4e3db827b12e5ea281b19eb03b6c71a1100
faizbyp <faizbyp@gmail.com>
2024-05-27 19:51:26 +0700

    change from termux on phone

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <small className="position-absolute top-0 end-0 px-4 py-3 text-white-50">Music Quote</small>
         <div className="row h-100 justify-content-center align-items-center">
           <figure id='quote-box' className="col-11 col-md-8 col-lg-6 col-xxl-4 bg-white px-4 py-3 rounded-4 shadow">
-            <h1 id="text">Teruslah mekar tanpa menjadi layu.</h1>
+            <h1 id="text">Change from vim.</h1>
             <p id='author' className="text-body-secondary">— Langit Biru Cinta Searah</p>
             <section id="buttons" className='text-end mb-2'>
               <a id='tweet-quote' href="https://twitter.com/intent/tweet" target='_blank' className='btn btn-secondary'>Share</a>
