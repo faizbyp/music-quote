80d307d26d21c0ba22390eb9f54e99c356f8f167
faizbyp <faizbyp@gmail.com>
2024-05-27 07:48:16 +0700

    initial commit + add font

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import '@fontsource-variable/inter';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <style>{`
+      body {
+        font-family: 'Inter Variable', sans-serif;
+      }
+    `}</style>
+    <App />
+  </React.StrictMode>,
+)
