32a3723b0919c134b0a4a365526000498db80ebf
faizbyp <faizbyp@gmail.com>
2024-05-28 18:35:06 +0700

    add quotes

--- src/quotes.js
+++ src/quotes.js
@@ -31,4 +31,52 @@ export const quotes = [
     quote: "I can be the one I want",
     author: "Running",
   },
+  {
+    quote: "It just so happens we're facing today for the first time",
+    author: "Cheers To Youth",
+  },
+  {
+    quote: "Janganlah kau pendam semuanya di dalam hati",
+    author: "Yuuhi Wo Miteiruka",
+  },
+  {
+    quote: "Walau seberat apapun, kesedihan tak berlanjut selamanya",
+    author: "Hanya Lihat Ke Depan",
+  },
+  {
+    quote: "Genggamlah selalu impianmu, sampai suatu saat mimpimu terkabul",
+    author: "River",
+  },
+  {
+    quote: "Jangan bandingkan jarak terbangnya, tapi bagaimana dan apa yang dilalui",
+    author: "Pesawat Kertas 365 Hari",
+  },
+  {
+    quote: "Kita ini insan bukan seekor sapi",
+    author: "Zona Nyaman",
+  },
+  {
+    quote: "Karena pelaut hebat tak pernah lahir di laut yang tenang",
+    author: "Jatuh, Bangkit Kembali!",
+  },
+  {
+    quote: "One day, you'll leave this world behind. So live a life you will remember",
+    author: "The Nights",
+  },
+  {
+    quote: "Someday you'll find that you are brighter than the star",
+    author: "Brand New Day",
+  },
+  {
+    quote: "Kita datang, kita lihat, kita menang",
+    author: "Meraih Bintang",
+  },
+  {
+    quote: "Be something greater, go make a legacy",
+    author: "High Hopes",
+  },
+  {
+    quote: "Lupakanlah semua yang lalu, kita sambut yang baru",
+    author: "Brand New Day",
+  },
 ];
