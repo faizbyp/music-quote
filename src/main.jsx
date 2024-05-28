e09b793beeacae98b921a5343b31dd5c607f4f1f
faizbyp <faizbyp@gmail.com>
2024-05-28 16:02:10 +0700

    change font to cdn

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '@fontsource-variable/inter';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <style>{`
-      body {
-        font-family: 'Inter Variable', sans-serif;
-      }
-    `}</style>
     <App />
   </React.StrictMode>,
 )
