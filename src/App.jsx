e084a33a3918e8e6c9fe19c5bf591a239d57c9c9
faizbyp <faizbyp@gmail.com>
2024-05-28 15:51:59 +0700

    separating data with app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
 import { useState } from "react";
+import { quotes } from "./quotes";
 
 function App() {
-  const quotes = [
-    {
-      quote: "Teruslah mekar tanpa menjadi layu",
-      author: "Langit Biru Cinta Searah"
-    },
-    {
-      quote: "Akan ada masa depan bagi semua yang bertahan",
-      author: "Satu-satu"
-    },
-    {
-      quote: "Kau terlalu berharga untuk luka",
-      author: "Diri"
-    },
-    {
-      quote: "What good is love and peace on earth, when it's exclusive?",
-      author: "Troubled Times"
-    },
-    {
-      quote: "Apapun yang terjadi, kita abadi",
-      author: "Apapun Yang Terjadi"
-    },
-    {
-      quote: "Kuncup yang berusaha keras pun akan mekar",
-      author: "Shonichi (Hari Pertama)"
-    }
-  ]
-
   const rand = Math.floor(Math.random() * quotes.length);
   const [quoteText, setQuoteText] = useState(quotes[rand].quote);
   const [authorText, setAuthorText] = useState(quotes[rand].author);
---
+++ src/quotes.js
@@ -0,0 +1,34 @@
+export const quotes = [
+  {
+    quote: "Teruslah mekar tanpa menjadi layu",
+    author: "Langit Biru Cinta Searah",
+  },
+  {
+    quote: "Akan ada masa depan bagi semua yang bertahan",
+    author: "Satu-satu",
+  },
+  {
+    quote: "Kau terlalu berharga untuk luka",
+    author: "Diri",
+  },
+  {
+    quote: "What good is love and peace on earth, when it's exclusive?",
+    author: "Troubled Times",
+  },
+  {
+    quote: "Apapun yang terjadi, kita abadi",
+    author: "Apapun Yang Terjadi",
+  },
+  {
+    quote: "Kuncup yang berusaha keras pun akan mekar",
+    author: "Shonichi (Hari Pertama)",
+  },
+  {
+    quote: "There ain't a thing that can stop me now",
+    author: "Flying High",
+  },
+  {
+    quote: "I can be the one I want",
+    author: "Running",
+  },
+];
