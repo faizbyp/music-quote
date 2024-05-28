e45a1d6dc0e5085c84494469667ccd0c89aa276a
faizbyp <faizbyp@gmail.com>
2024-05-28 15:35:50 +0700

    randomize work, add quotes, share query work, change title name

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   const quotes = [
     {
@@ -7,23 +9,44 @@ function App() {
     {
       quote: "Akan ada masa depan bagi semua yang bertahan",
       author: "Satu-satu"
+    },
+    {
+      quote: "Kau terlalu berharga untuk luka",
+      author: "Diri"
+    },
+    {
+      quote: "What good is love and peace on earth, when it's exclusive?",
+      author: "Troubled Times"
+    },
+    {
+      quote: "Apapun yang terjadi, kita abadi",
+      author: "Apapun Yang Terjadi"
+    },
+    {
+      quote: "Kuncup yang berusaha keras pun akan mekar",
+      author: "Shonichi (Hari Pertama)"
     }
   ]
 
+  const rand = Math.floor(Math.random() * quotes.length);
+  const [quoteText, setQuoteText] = useState(quotes[rand].quote);
+  const [authorText, setAuthorText] = useState(quotes[rand].author);
+  
   const handleRand = () => {
-    alert("Randomize not working yet");
+    const rand = Math.floor(Math.random() * quotes.length);
+    setQuoteText(quotes[rand].quote);
+    setAuthorText(quotes[rand].author);
   }
-
 
   return (
     <main className='container-fluid bg-success position-relative' style={{height: "100svh"}}>
       <small className="position-absolute top-0 end-0 px-4 py-3 text-white-50">Music Quote</small>
         <div className="row h-100 justify-content-center align-items-center">
           <figure id='quote-box' className="col-11 col-md-8 col-lg-6 col-xxl-4 bg-white px-4 py-3 rounded-4 shadow">
-            <h1 id="text">{quotes[1].quote}</h1>
-            <p id='author' className="text-body-secondary">{`— ${quotes[1].author}`}</p>
+            <h1 id="text">{quoteText}</h1>
+            <p id='author' className="text-body-secondary">{`— ${authorText}`}</p>
             <section id="buttons" className='text-end mb-2'>
-              <a id='tweet-quote' href="https://twitter.com/intent/tweet" target='_blank' className='btn btn-secondary'>Share</a>
+              <a id='tweet-quote' href={`https://twitter.com/intent/tweet?text=${quoteText}%0A— ${authorText}%0Ahttps://music-quote.vercel.app/`} target='_blank' className='btn btn-secondary'>Share</a>
               <button id='new-quote' className='btn btn-success ms-3' onClick={handleRand}>Randomize</button>
             </section>
           </figure>
